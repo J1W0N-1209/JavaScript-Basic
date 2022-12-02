e9174de7d7b415ec92e19f6d815823fa86289f85
jiwon <nabook754@naver.com>
2022-12-02 16:48:02 +0900

    변수와 상수

---
+++ 1-3.js
@@ -0,0 +1,16 @@
+// 변수
+// 담긴  값이 바뀔 수 있는 주머니
+// let 사용
+
+let x;
+console.log(x);
+x = 1;
+console.log(x);
+
+// 상수
+// 담긴 값이 바꿀 수 없는 주머니
+// const 사용
+// 흔히 대문자로 명명
+
+const PI = 3.14;
+console.log('원주율:', PI);
